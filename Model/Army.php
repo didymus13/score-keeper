db35f48a4e49655da45417b57ecdea31d967d9d6
Stephane Doiron <stephane@stephanedoiron.com>
2013-10-05 00:12:45 -0400

    Armies enabled

---
+++ Model/Army.php
@@ -0,0 +1,23 @@
+<?php
+
+class Army extends AppModel {
+    public $table = 'armies';
+    
+    public $validate = array(
+        'user_id' => array(
+            'rule' => 'numeric',
+            'required' => true
+        ),
+        'name' => array(
+            'rule' => 'notEmpty',
+            'required' => true
+        ),
+        'description' => 'notEmpty',
+        'is_active' => 'numeric'
+    );
+    
+    public $belongsTo = array('User');
+    public $hasAndBelongsToMany = array('Campaign');
+}
+
+?>
